4558eb9282d815f4c8b1f302c920c91648e3b248
Vukasin1996 <root@localhost.localdomain>
2020-04-09 14:22:46 -0400

    formater broja, novi commit

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # node-cli
 
-### `npm install`
-for install package
-
 ### `node index`
-for start project 
+
+for start project
--- index.js
+++ index.js
@@ -1,20 +1,99 @@
-const http = require("http");
-//const chalk = require("chalk");
-//const clear = require("clear");
-const figlet = require("figlet");
+var JFile = require("jfile");
 
-//numbers for formatting
-const rez = "123456789";
+var one = ",,,,,|,,|";
+var two = ",_,,_||_,";
+var three = ",_,,_|,_|";
+var four = ",,,|_|,,|";
+var five = ",_,|_,,_|";
+var six = ",_,|_,|_|";
+var seven = ",_,,,|,,|";
+var eight = ",_,|_||_|";
+var nine = ",_,|_|,_|";
 
-//formatting numbers
-const showRez = figlet.textSync(rez, { horizontalLayout: "full" });
-console.log(rez);
+var myF = new JFile("textCod.txt");
 
-//showed formatted numbers in browser
-let app = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end(showRez);
-});
+function FormattingNum(
+  num1,
+  num2,
+  num3,
+  num4,
+  num5,
+  num6,
+  num7,
+  num8,
+  num9,
+  failNum
+) {
+  var fullNumShow = [];
+  const LFail = failNum.lines.length;
+  const a1 = failNum.lines[0] ? failNum.lines[0].length : 0;
+  const a2 = failNum.lines[1] ? failNum.lines[1].length : 0;
+  const a3 = failNum.lines[2] ? failNum.lines[2].length : 0;
+  if (
+    LFail < 3 ||
+    LFail > 3 ||
+    a1 < 27 ||
+    a1 > 27 ||
+    a2 > 27 ||
+    a2 < 27 ||
+    a3 > 27 ||
+    a3 < 27
+  ) {
+    console.log("formatter does not support the text you entered");
+  } else {
+    for (s = 3; s < 28; s++) {
+      for (i = 0; i < 25; i++) {
+        const myOne = failNum.lines[0]
+          .toString()
+          .substring(s, i)
+          .replace(/\s/g, ",");
+        const myTwo = failNum.lines[1]
+          .toString()
+          .substring(s, i)
+          .replace(/\s/g, ",");
+        const myThree = failNum.lines[2]
+          .toString()
+          .substring(s, i)
+          .replace(/\s/g, ",");
 
-app.listen(4000, "127.0.0.1");
-console.log("Node server running on port 4000");
+        fillNum = myOne + myTwo + myThree;
+        switch (fillNum) {
+          case num1:
+            fullNumShow.push("1");
+            break;
+          case num2:
+            fullNumShow.push("2");
+            break;
+          case num3:
+            fullNumShow.push("3");
+            break;
+          case num4:
+            fullNumShow.push("4");
+            break;
+          case num5:
+            fullNumShow.push("5");
+            break;
+          case num6:
+            fullNumShow.push("6");
+            break;
+          case num7:
+            fullNumShow.push("7");
+            break;
+          case num8:
+            fullNumShow.push("8");
+            break;
+          case num9:
+            fullNumShow.push("9");
+            break;
+          default:
+        }
+        i += 2;
+        s += 3;
+      }
+    }
+    const showNumber = fullNumShow.toString().replace(/,/g, "");
+
+    console.log(showNumber);
+  }
+}
+FormattingNum(one, two, three, four, five, six, seven, eight, nine, myF);
